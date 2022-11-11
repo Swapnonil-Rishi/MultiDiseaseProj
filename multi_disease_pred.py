376d42444ceac4709d0dffe0e964d2d6f263c391
Swapnonil-Rishi <86824981+Swapnonil-Rishi@users.noreply.github.com>
2022-11-11 18:16:35 +0530

    Update multi_disease_pred.py

--- multi_disease_pred.py
+++ multi_disease_pred.py
@@ -14,7 +14,7 @@ heart_model=pickle.load(open('fin_mod_heart.sav','rb'))
 diabe_model=pickle.load(open('fin_mod_diabetes.sav','rb'))
 parkins_model=pickle.load(open('parkinsons_model.sav','rb'))
 
-st.title('Multiple Disease Prediction Syste')
+st.title('Multiple Disease Prediction System')
 
     
 tab1, tab2, tab3 = st.tabs(["Heart Disease Report", "Diabates Report", "Perkinsons Report"],
